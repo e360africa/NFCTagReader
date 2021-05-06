a1c2a189de1d6ac1f62baaf8a9bb6b042003ccbd
Mbuodile Obiosio <cazewonder@gmail.com>
2021-05-06 16:57:15 +0100

    Initial commit

---
+++ app/src/main/java/com/e360africa/nfctagreader/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.e360africa.nfctagreader
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
